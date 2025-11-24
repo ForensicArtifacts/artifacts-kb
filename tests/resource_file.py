713f995569277f3663060db17b1eefdf9e355a94
Joachim Metz <joachim.metz@gmail.com>
2025-11-24 14:49:13 +0100

    Changes for release and deployment (#32)

--- tests/resource_file.py
+++ tests/resource_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Tests for the Windows Message Resource (WRC) file class."""
 
--- tools/generate_docs.py
+++ tools/generate_docs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Script to generate artifact definition documentation."""
 
